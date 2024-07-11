eb198728403ca38e79502e660cd773635be27151
jdeslaur <justin.deslauriers@gmail.com>
2024-07-11 16:27:50 -0700

    Add files via upload

---
+++ string_cluster.py
@@ -0,0 +1,46 @@
+import json
+import csv
+import numpy as np
+from tqdm import tqdm
+from sklearn.cluster import DBSCAN
+from fuzzywuzzy import fuzz
+
+filePath = '/Users/justin/Documents/user_entities_clean'
+userStrings = []
+
+""" with open(filePath, 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        userStrings.append(row['user']) """
+
+with open(filePath, 'r') as file:
+    total_lines = sum(1 for line in file)
+with open(filePath, 'r') as file:
+    for line in tqdm(file, total=total_lines, desc="Processing lines"):
+        userStrings.append(line.strip())
+    
+
+with tqdm(total=len(userStrings) * len(userStrings), desc="Calculating similarity matrix") as pbar:
+    for i in range(len(userStrings)):
+        for j in range(len(userStrings)):
+            # Calculate similarity (replace this with your actual similarity calculation)
+            similarity_matrix = np.zeros((len(userStrings), len(userStrings)))
+            
+            # Update the progress bar
+            pbar.update(1)
+
+for i in range(len(userStrings)):
+    for j in range(len(userStrings)):
+        similarity_matrix[i][j] = fuzz.ratio(userStrings[i],userStrings[j])
+
+distance_matrix = 100 - similarity_matrix
+epsValues = [40,50,60]
+for epsValue in epsValues:
+    clustering = DBSCAN(eps=epsValue, min_samples=2, metric='precomputed').fit(distance_matrix)
+    outputFilePath = f'/Users/justin/Downloads/clustered_user_{epsValue}.csv'
+    with open(outputFilePath, 'w', newline='') as csvfile:
+        fieldNames = ['user','cluster']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldNames)
+        writer.writeheader()
+        for i, label in enumerate(clustering.labels_):
+           writer.writerow({'user': userStrings[i], 'cluster':label})
